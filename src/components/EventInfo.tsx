51b352abfd1e3eeac9bb4ae2c3006f449c75e5a4
rocionacul <rocionacul@gmail.com>
2026-02-15 13:52:42 -0300

    new color aesthetic

--- src/components/EventInfo.tsx
+++ src/components/EventInfo.tsx
@@ -57,7 +57,7 @@ export const EventInfo: React.FC = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
               <h3 className="event-card-title">Vibe</h3>
-              <p className="event-card-text">Saltos, Musica & Diversion</p>
+              <p className="event-card-text">Saltos, Pop Punk & Diversion</p>
               <p className="event-card-subtext">Comida & Bebidas (sin alcohol) incluidas</p>
             </div>
           </div>
